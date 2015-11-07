e0bb849f79532078950ce0c9ae181ce79067b987
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-11-06 19:32:06 -1000

    initial matchdata sorting algorithim complete

---
+++ sortingAlgorithims.js
@@ -0,0 +1,58 @@
+var riotMatchData = {}; //insert Riot's Match Data here
+var parsedMatchData = {};
+
+for(var p = 1; p < riotMatchData.participants.length + 1; p++) {
+    parsedMatchData['player'+p] = [];
+    var timeline = riotMatchData.timeline.frames;
+    for(var i = 0; i < timeline.length; i++) {
+        var data = [];
+        data.push(timeline[i].timestamp);
+        data.push('Position');
+        data.push(timeline[i].participantFrames[p].position.x);
+        data.push(timeline[i].participantFrames[p].position.y);
+        parsedMatchData['player'+p].push(data);
+        if(timeline[i].events !== undefined) {
+            for (var j = 0; j < timeline[i].events.length; j++) {
+                var data = [];
+                var eventFrame = timeline[i].events[j];
+                if(eventFrame.eventType == 'CHAMPION_KILL') {
+                    if(eventFrame.killerId == p) {
+                        data.push(eventFrame.timestamp);
+                        data.push('Enemy Slain');
+                        data.push(eventFrame.position.x);
+                        data.push(eventFrame.position.y);
+                        parsedMatchData['player'+p].push(data);
+                    } else if(eventFrame.victimId == p) {
+                        data.push(eventFrame.timestamp);
+                        data.push('Player Slain');
+                        data.push(eventFrame.position.x);
+                        data.push(eventFrame.position.y);
+                        parsedMatchData['player'+p].push(data);
+                    } else if(eventFrame.assistingParticipantIds.indexOf(p) !== -1) {
+                        data.push(eventFrame.timestamp);
+                        data.push('Kill Assisted');
+                        data.push(eventFrame.position.x);
+                        data.push(eventFrame.position.y);
+                        parsedMatchData['player'+p].push(data);
+                    }
+                } else if (eventFrame.eventType == 'BUILDING_KILL') {
+                    if(eventFrame.killerId == p) {
+                        data.push(eventFrame.timestamp);
+                        data.push('Building Destroyed');
+                        data.push(eventFrame.position.x);
+                        data.push(eventFrame.position.y);
+                        parsedMatchData['player'+p].push(data);
+                    }else if(eventFrame.assistingParticipantIds.indexOf(p) !== -1) {
+                        data.push(eventFrame.timestamp);
+                        data.push('Building Destruction Assisted');
+                        data.push(eventFrame.position.x);
+                        data.push(eventFrame.position.y);
+                        parsedMatchData['player'+p].push(data);
+                    }
+                }
+            }
+        }
+
+    }
+}
+
