4bd24a99b70e1cc863baa96aa76fd206a1af7431
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2015-11-09 12:22:49 -1000

    add recentMatchController and matchController, modify service functions, modify amgular routes on app.js

--- public/js/services.js
+++ public/js/services.js
@@ -2,9 +2,16 @@
 angular.module('laneApp')
   .service('summonerInfo', [ '$http', function($http) {
     this.getSummonerInfo = function() {
-      console.log($http.get('http://localhost:3000/getSummoner'));
       return $http.get('http://localhost:3000/getSummoner');
     };
-
+  }])
+  .service('recentMatchInfo', [ '$http', function($http) {
+    this.getRecentMatchInfo = function() {
+      return $http.get('http://localhost:3000/getRecentMatch');
+    };
+  }])
+  .service('matchInfo', [ '$http', function($http) {
+    this.getMatchInfo = function() {
+      return $http.get('http://localhost:3000/getMatch');
+    };
   }]);
-  // .service('recentMatchInfo', []);
