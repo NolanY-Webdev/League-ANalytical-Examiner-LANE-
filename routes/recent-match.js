cdcb689b6e04bbc300ad8382e661b4ad88f8eb2c
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2015-11-18 15:06:54 -1000

    modify recent match js and view

--- routes/recent-match.js
+++ routes/recent-match.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var champion = require('../championSelect');
 
 router
   .route('/')
@@ -21,6 +22,26 @@ router
       if (err) {
         return res.status(500).json(err);
       }
+   // prune body
+      var parsed = JSON.parse(body);
+      console.log('this is the body',parsed);
+
+      // loop through array of participants
+      for (var i = 0; i < parsed.games.length; i++) {
+        // loop through champion array on champtionSelect.js
+        for (var j = 0; j < champion.length; j++) {
+          // checks if participants
+          // console.log(parsed.participants[i].championId);
+          // console.log(champion[j].key);
+
+          if (parsed.games[i].championId == champion[j].key) {
+            parsed.games[i].championImage = champion[j].image;
+            console.log('championId: ', champion[j].key);
+            console.log(parsed.games[i].championImage = champion[j].image);
+          }
+        }
+      }
+
       res.send(body);
     });
   });
